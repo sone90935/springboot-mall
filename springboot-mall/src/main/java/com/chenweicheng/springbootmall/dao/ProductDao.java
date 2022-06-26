0c58d1696f3b252b11a533eb3da0816139ac6b0b
sone90935 <sone90935@gmail.com>
2022-06-26 19:24:21 +0800

    商品功能 實作查詢商品

---
+++ springboot-mall/src/main/java/com/chenweicheng/springbootmall/dao/ProductDao.java
@@ -0,0 +1,10 @@
+package com.chenweicheng.springbootmall.dao;
+
+import com.chenweicheng.springbootmall.model.Product;
+
+public interface ProductDao {
+
+    Product getProductById(Integer productId);
+
+
+}
---
+++ springboot-mall/src/main/java/com/chenweicheng/springbootmall/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.chenweicheng.springbootmall.service;
+
+import com.chenweicheng.springbootmall.model.Product;
+
+public interface ProductService {
+
+    Product getProductById(Integer productId);
+}
