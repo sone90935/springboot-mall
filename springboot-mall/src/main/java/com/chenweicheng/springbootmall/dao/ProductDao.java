7908a107a75330165c8753540e6b44df15ce30cc
sone90935 <sone90935@gmail.com>
2022-06-27 21:10:24 +0800

    商品功能 實作查詢商品列表(基礎實作)

--- springboot-mall/src/main/java/com/chenweicheng/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/chenweicheng/springbootmall/dao/ProductDao.java
@@ -3,7 +3,16 @@ package com.chenweicheng.springbootmall.dao;
 import com.chenweicheng.springbootmall.dto.ProductRequest;
 import com.chenweicheng.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+
+     List<Product> getProducts();
+
+
+
+
 
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
--- springboot-mall/src/main/java/com/chenweicheng/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/chenweicheng/springbootmall/service/ProductService.java
@@ -3,8 +3,11 @@ package com.chenweicheng.springbootmall.service;
 import com.chenweicheng.springbootmall.dto.ProductRequest;
 import com.chenweicheng.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
 
