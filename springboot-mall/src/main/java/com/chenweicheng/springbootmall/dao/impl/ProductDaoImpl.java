46b30625590f4e30fa48ed3bc9f01b30e9e61773
sone90935 <sone90935@gmail.com>
2022-06-28 04:15:36 +0800

    商品功能 提煉程式

--- springboot-mall/src/main/java/com/chenweicheng/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/chenweicheng/springbootmall/dao/impl/ProductDaoImpl.java
@@ -31,16 +31,7 @@ public class ProductDaoImpl implements ProductDao {
         Map<String,Object> map=new HashMap<>();
 
         //查詢條件
-        if(productQueryParams.getCategory()!=null){
-            sql=sql+" AND category=:category";
-            map.put("category",productQueryParams.getCategory().name());
-        }
-
-        if(productQueryParams.getSearch()!=null){
-            sql=sql+" AND product_name LIKE:search";
-            map.put("search","%"+productQueryParams.getSearch()+"%");
-        }
-
+        sql = addFilterSql(sql, map, productQueryParams);
      Integer total =   namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
 
         return total;
@@ -54,15 +45,7 @@ public class ProductDaoImpl implements ProductDao {
         Map<String,Object> map=new HashMap<>();
 
         //查詢條件
-        if(productQueryParams.getCategory()!=null){
-            sql=sql+" AND category=:category";
-            map.put("category",productQueryParams.getCategory().name());
-        }
-
-        if(productQueryParams.getSearch()!=null){
-            sql=sql+" AND product_name LIKE:search";
-            map.put("search","%"+productQueryParams.getSearch()+"%");
-        }
+        sql = addFilterSql(sql, map, productQueryParams);
 
         //排序
         sql=sql+" ORDER BY "+productQueryParams.getOrderBy()+" "+productQueryParams.getSort();
@@ -157,5 +140,18 @@ public class ProductDaoImpl implements ProductDao {
 
         namedParameterJdbcTemplate.update(sql,map);
 
+    }
+
+    private String addFilterSql(String sql, Map<String,Object> map, ProductQueryParams productQueryParams){
+        if(productQueryParams.getCategory()!=null){
+            sql=sql+" AND category=:category";
+            map.put("category",productQueryParams.getCategory().name());
+        }
+
+        if(productQueryParams.getSearch()!=null){
+            sql=sql+" AND product_name LIKE:search";
+            map.put("search","%"+productQueryParams.getSearch()+"%");
+        }
+        return sql;
     }
 }
