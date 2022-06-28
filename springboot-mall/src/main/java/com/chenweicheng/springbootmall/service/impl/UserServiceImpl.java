a538d0c06a871947503aac196845ba911beb7720
sone90935 <sone90935@gmail.com>
2022-06-28 16:01:32 +0800

    帳號功能 檢查註冊的email 隱藏返回的密碼

--- springboot-mall/src/main/java/com/chenweicheng/springbootmall/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/com/chenweicheng/springbootmall/service/impl/UserServiceImpl.java
@@ -4,12 +4,19 @@ import com.chenweicheng.springbootmall.dao.UserDao;
 import com.chenweicheng.springbootmall.dto.UserRegisterRequest;
 import com.chenweicheng.springbootmall.model.User;
 import com.chenweicheng.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
 
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserDao userDao;
 
@@ -20,6 +27,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        //檢查註冊的email
+       User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+       if(user!=null){
+           log.warn("該email{}已經被註冊",userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+       }
+
+       //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 }
